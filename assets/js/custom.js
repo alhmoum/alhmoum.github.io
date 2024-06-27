e3c75cd5d5e91bdf780157fb71e164dbfbc2af1a
Lhassan Alhmoum <grafiprod.dev@gmail.com>
2024-06-27 09:19:53 +0200

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -31,3 +31,36 @@ document.addEventListener('DOMContentLoaded', function() {
     headroom.init();
   
 });//End of the DOM
+
+(function ($) {
+
+    "use strict";
+  
+      // COLOR MODE
+      
+      // HEADER
+      //$(".navbar").headroom();
+  
+      // PROJECT CAROUSEL
+      $('.owl-carousel').owlCarousel({
+          items: 1,
+          loop:true,
+          margin:10,
+          nav:true
+      });
+  
+      // SMOOTHSCROLL
+      $(function() {
+        $('.nav-link, .custom-btn-link').on('click', function(event) {
+          var $anchor = $(this);
+          $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+          event.preventDefault();
+        });
+      });  
+  
+      // TOOLTIP
+      $('.social-links a').tooltip();
+  
+  })(jQuery);
